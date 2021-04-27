718aa46da4086d5fdf202f49dd9b29489b014d29
Lara <75126217+little-evil-genius@users.noreply.github.com>
2021-04-27 11:16:00 +0200

    Create shortsearch.lang.php

---
+++ inc/languages/deutsch_du/shortsearch.lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$l['shortsearch'] = "Kurzgesuche";
+$l['shortsearch_add'] = "Kurzgesuch hinzufügen";
+$l['shortsearch_add_category'] = "Kategorie des Kurzgesuch";
+$l['shortsearch_add_category_desc'] = "Die Kurzgesuche sind nach Kategorien georndet, in welche passt dein Kurgesuch am besten? Wenn dir eine Kategorie fehlt, dann sag einem Teammitglied bescheid.";
+$l['shortsearch_add_title'] = "Titel des Kurzgesuch";
+$l['shortsearch_add_title_desc'] = "Auch die Kurzgesuche haben einen (kurzen) schönen Titel. Wie lautet der von deinem Gesuch?";
+$l['shortsearch_add_relation'] = "Beziehung zueinander";
+$l['shortsearch_add_relation_desc'] = "Wie kann man die Beziehung zwischen deinem Charakter und der gesuchten Person bezeichnen? Hier soll ein kurzes Schlagwort stehen.";
+$l['shortsearch_add_age'] = "Alter";
+$l['shortsearch_add_age_desc'] = "Wie alt ist die gesuchte Person? Du kannst auch eine Altersspanne angeben.";
+$l['shortsearch_add_gender'] = "Geschlecht";
+$l['shortsearch_add_gender_desc'] = "Welches Geschlecht gehört die gesuchte Person an?";
+$l['shortsearch_add_job'] = "Beruf";
+$l['shortsearch_add_job_desc'] = "Mit was verdient die gesuchte Person ihr Geld oder geht die Person noch zur Schule?";
+$l['shortsearch_add_relationstatus'] = "Beziehungstatus";
+$l['shortsearch_add_relationstatus_desc'] = "Welchen Beziehungsstatus hat die gesuchte Person?";
+$l['shortsearch_add_avatar'] = "Avatarpersonvorschlag";
+$l['shortsearch_add_avatar_desc'] = "Falls dir du eine gewisse Avatarperson im Kopf hast für die gesuchte Person kannst du sie hier vorschlagen. Mehrere Vorschläge sind auch möglich.";
+$l['shortsearch_add_text'] = "Beschreibung";
+$l['shortsearch_add_text_desc'] = "Neben den ganzen oberflächigen Fakten solltest du noch einen ausführlichen Beschreibungstext rund um die gesuchte Person schreiben.";
+$l['shortsearch_edit'] = "Kurzgesuch bearbeiten";
+$l['shortsearch_own'] = "Eigene Kurzgesuche";
+$l['shortsearch_alert'] = "Es gibt neue <a href=\"misc.php?action=shortsearch\"><b>Kurzgesuche</b></a>. Vielleicht ist was für dich dabei.";
+$l['shortsearch_pm_user'] = "Dein Kurzgesuch wurde von mir reserviert.<br> Vielleich können wir mal drüber sprechen, was wir aus dieser Idee so machen können.";
+$l['shortsearch_pm_guest'] = "Dein Kurzgesuch wurde von einem Gast reserviert.<br> Hoffentlich meldet sich der Gast bei dir oder im Board an. Wenn es leider nicht dazu kommt, darfst du nach zwei Wochen das Kurzgesuch wieder freigeben.";
+
+?>
